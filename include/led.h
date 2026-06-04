5b2bc31ac252de3775bbb72c39f4a303b8ec5f53
antoha-ndd <antoha-ndd@mail.ru>
2026-06-04 09:03:57 +0200

    1

--- include/led.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "outputdevice.h"
-
-class TLed : public TOutputDevice
-{
-public:
-    TLed( uint8_t _Pin) : TOutputDevice(_Pin){};
-};
--- include/relay.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "outputdevice.h"
-
-class TRelay : public TOutputDevice
-{
-public:
-    TRelay(TObject *_Parent, uint8_t _Pin) : TOutputDevice(_Parent, _Pin){};
-};
